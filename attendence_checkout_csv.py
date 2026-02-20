a833945d26d56f1be4fef24cbfa4e09a97152adc
Jyotiraditya Khatua <jyotiraditya20122004@gmail.com>
2026-02-20 08:38:08 +0530

    Add attendance checkout CSV file

---
+++ attendence_checkout_csv.py
@@ -0,0 +1,3 @@
+name,roll_no,email,date,check_out_time
+jyotiraditya,2200951530014,pratap chandra khatua@gmail.com,2026-02-14,20:11:24
+mohit harma,2200951530015,mouitdfr@gmail.com,2026-02-14,20:50:34
